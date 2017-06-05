55d379ec917ba66546e2e6fc72a7f3ed8205a891
Jacob Selig <jselig@fanatics.com>
2017-06-05 08:36:13 -0600

    exporting stars 💫

--- src/index.js
+++ src/index.js
@@ -1,38 +1,9 @@
-import ʹmap from 'ramda/src/map'
-import ʹchain from 'ramda/src/chain'
-import {
-  rail as ʹrail,
-  handrail as ʹhandrail
-} from './handrail'
+export {map, chain} from 'ramda'
 
-import {
-  Right as ʹRight,
-  Left as ʹLeft,
-  GuidedRight as ʹGuidedRight,
-  GuidedLeft as ʹGuidedLeft,
-  isRight as ʹisRight,
-  isLeft as ʹisLeft,
-  fold as ʹfold,
-  ap as ʹap,
-  bimap as ʹbimap
-} from './util'
+export * from './handrail'
+export * from './util'
 
-export const rail = ʹrail
-export const handrail = ʹhandrail
-
-export const Right = ʹRight
-export const Left = ʹLeft
-export const GuidedRight = ʹGuidedRight
-export const GuidedLeft = ʹGuidedLeft
-export const isRight = ʹisRight
-export const isLeft = ʹisLeft
-export const chain = ʹchain
-export const map = ʹmap
-export const fold = ʹfold
-export const ap = ʹap
-export const bimap = ʹbimap
-
-// extended metaphor API
-export const baluster = rail
-export const balustrade = handrail
-export const net = fold
+// Extended metaphor API
+export {rail as baluster} from './handrail'
+export {handrail as balustrade} from './handrail'
+export {fold as net} from './util'
